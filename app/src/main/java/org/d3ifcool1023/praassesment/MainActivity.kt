34e568429889e2ca3c8e3cc723dcf7180f861764
isubhakti <imamsubhakti@gmail.com>
2020-03-04 11:14:54 +0700

    test commit

---
+++ app/src/main/java/org/d3ifcool1023/praassesment/MainActivity.kt
@@ -0,0 +1,29 @@
+package org.d3ifcool1023.praassesment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import org.d3ifcool1023.praassesment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+        //dd
+    }
+}
